059e5843e9c3dc8ba602e81417d0b5515f1eb80f
Alex Kahaer <AlexKahaer@gmail.com>
2022-05-17 18:25:03 -0700

    case 3 created

---
+++ src/test/java/com/cydeo/test/ExtraPractices/Case3.java
@@ -0,0 +1,19 @@
+package com.cydeo.test.ExtraPractices;
+
+public class Case3 {
+    /*Test Case #3
+Test data: helpdesk51@cybertekschool.com
+Category: Negative Test Case
+Environment: https://login2.nextbasecrm.com/auth/?backurl=%2Fstream%2F
+
+Given I am on NextBaseCRM Login page
+When I enter invalid help desk username
+And I enter invalid help desk password
+And I click log in button
+Then I should see error message
+
+     */
+
+
+
+}
