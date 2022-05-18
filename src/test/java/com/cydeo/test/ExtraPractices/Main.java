184bf6c96664857d07c711ac11dd61f802ecbd98
AhmetDulgeroglu <AhmetDulgeroglu111@gmail.com>
2022-05-18 17:19:21 -0400

    Ahmet's class

--- src/test/java/com/cydeo/test/ExtraPractices/Main.java
+++ src/test/java/com/cydeo/test/ExtraPractices/Main.java
@@ -1,11 +1,48 @@
 package com.cydeo.test.ExtraPractices;
 
-public class Main {
-    String validUsername = "helpdesk51@cybertekschool.com";
-    String invalidUsername = "invalidUsername@cybertekschool.com";
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-    String validPassword = "UserUser";
-    String invalidPassword = "invalidPassword";
+public class Main {
+
+//------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://login.nextbasecrm.com/");
+
+
+        String validUsername = "helpdesk51@cybertekschool.com";
+        String invalidUsername = "invalidUsername@cybertekschool.com";
+
+        String validPassword = "UserUser";
+        String invalidPassword = "invalidPassword";
+        String expected="Incorrect login or password";
+         /*
+    Test Case 1:
+
+    */
+
+
+
+    /*
+
+    test Case 2
+
+
+
+     */
+
+        TestCase2.loginInvalidPassword(driver, invalidPassword,validUsername);
+
+        TestCase2.checkErrorMSG(driver, expected);
+
+
+
+    }
+
 
 
 }
--- src/test/java/com/cydeo/test/ExtraPractices/TestCase2.java
+++ src/test/java/com/cydeo/test/ExtraPractices/TestCase2.java
@@ -9,24 +9,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestCase2 {
 
-    public static void loginInvalidPassword(String validUsername, String invalidPassword){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-
-        driver.get("https://login.nextbasecrm.com/");
+    public static void loginInvalidPassword(WebDriver driver, String invalidPassword, String validUsername){
 
         WebElement login= driver.findElement(By.name("USER_LOGIN"));
         login.sendKeys(validUsername);
 
         WebElement pass= driver.findElement(By.name("USER_PASSWORD"));
         pass.sendKeys(invalidPassword+ Keys.ENTER);
+    }
 
+    public static void checkErrorMSG(WebDriver driver, String expected){
+        WebElement msg=driver.findElement(By.className("errortext"));
+        //Created msg web element to find class name.
+        String actual= msg.getText();
 
+        //Created actual result , and expected result
 
-
-        System.out.println();
-        driver.close();
+        if (actual.equals(expected)){
+            System.out.println("TEST CASE PASSED");
+        }else{
+            System.out.println("Test Case FAILED");
+        }
 
     }
 }
